a19562d35f578778021c3b2dd250af9e7ac0f305
OrregoMatias <orregomatias50@gmail.com>
2024-05-03 10:06:07 -0300

    Termine el archivo parImpar.js

---
+++ assets/parImpar.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("Ingrese el numero que desee saber:"))
+resultnum = num%2;
+if(resultnum > 0){
+    console.log("El numero es Impar")
+}else if(num=String){
+    console.log("No es un numero")
+}else{
+    console.log("El numero es Par")
+}
