017f5cd3c0f08ac0488abeec9876ea7d3ae8c619
OrregoMatias <orregomatias50@gmail.com>
2024-05-03 09:48:38 -0300

    se termino el archivo JS rangoEdad

---
+++ assets/rangoEdad.js
@@ -0,0 +1,13 @@
+let edad = parseInt(prompt("Ingrese su edad"))
+if (edad>=0 && edad<=12){
+console.log("Su rango de edad es: niño")
+} else if (edad>=13 && edad<=19){
+console.log("Su rango de edad es: adolecente")
+}else if (edad>=20 && edad<=59){
+console.log("Su rango de edad es: adulto")
+}else if (edad = String){
+console.log("No es un numero")
+}
+else{
+console.log("Su rango de edad es: adulto mayor")
+}
