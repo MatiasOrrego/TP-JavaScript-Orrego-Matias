d3c7feba39c37eb8569ea1701260dcb2dc7908f3
MatiasOrrego <orregomatias50@gmail.com>
2024-05-04 20:35:32 -0300

    Termine el archivo diaSemana.js

---
+++ assets/diaSemana.js
@@ -0,0 +1,18 @@
+let dia = parseInt (prompt("Ingres un numero del 1 a 7:"))
+if (dia==1) {
+    console.log("El dia es Lunes")
+} else if (dia==2) {
+    console.log("El dia es Martes")
+} else if (dia==3) {
+    console.log("El dia es Miercoles")
+} else if (dia==4) {
+    console.log("El dia es Jueves")
+} else if (dia==5) {
+    console.log("El dia es Viernes")
+} else if (dia==6) {
+    console.log("El dia es Sabado")
+} else if (dia==7) {
+    console.log("El dia es Domingo")
+} else {
+    console.log ("Error.")
+}
