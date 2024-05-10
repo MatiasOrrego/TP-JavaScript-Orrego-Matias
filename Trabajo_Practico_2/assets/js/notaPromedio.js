c93eccf98d00be63b7b10f33ad833fc2567e05e8
OrregoMatias <orregomatias50@gmail.com>
2024-05-10 10:30:23 -0300

    Los errores de la primera actividad fueron correjidos

--- Trabajo_Practico_2/assets/js/notaPromedio.js
+++ Trabajo_Practico_2/assets/js/notaPromedio.js
@@ -2,27 +2,22 @@ let trimestre1 = parseInt(prompt("Ingrese la nota del primer trimestre:"));
 let trimestre2 = parseInt(prompt("Ingrese la nota del segundo trimestre:"));
 let trimestre3 = parseInt(prompt("Ingrese la nota del tercer trimestre:"));
 let promedio = (trimestre1 + trimestre2 + trimestre3) / 3;
-switch (promedio <0 && promedio >10) {
-    case 1:
-    case 2:
-    case 3:
+switch (true) {
+    case promedio<=3 && promedio>=1:
     console.log ("Nota Insuficiente")
     break
-    case 4:
-    case 5:
+    case promedio == 4 || promedio == 5:
     console.log ("Nota Regular")
     break
-    case 6:
-    case 7:
+    case promedio == 6 || promedio == 7:
     console.log ("Nota Buena")
     break
-    case 8:
-    case 9:
+    case promedio == 8 || promedio == 9:
     console.log ("Nota Muy Buena")
     break
-    case 10:
+    case promedio == 10:
     console.log ("Nota Sobresaliente")
-        break;
+    break;
     default:
         console.log ("El promedio no pudo ser calculado, debido a los valores ingresados.")
         break;
