b365203beb92cc22d15994ba4b15002218b2f77b
MatiasOrrego <orregomatias50@gmail.com>
2024-05-20 21:01:03 -0300

    logre agregar la funcion añadir tarea

--- Tp-AdminTask-OrregoMatias/assets/js/app.js
+++ Tp-AdminTask-OrregoMatias/assets/js/app.js
@@ -1,52 +1,18 @@
-// Material de ayuda para resolver la actividad practica.
-
-// Funciones
-// las funciones en JavaScript son como pequeñas cajas que contienen un conjunto de instrucciones 
-// que pueden ser ejecutadas en cualquier momento.
-
-// Aquí, sumar es el nombre de la función. a y b son los parámetros que la función espera recibir, y return a + b es 
-function sumar(a, b) {
-    return a + b
-}
-
-// lo que la función devuelve cuando es llamada. Entonces, si llamas a la función sumar(3, 5), devolverá 8, porque suma 3 y 5. 
-sumar(3, 5) // 8
-
-// Expresiones regulares
-
-// Expresion regular que valida que no contenga espacios en blanco al principio o al final ni que este vacio.
-const regex = /^\S+$/;
-
-// Se utiliza el metodo test y se le pasa como parametro la variable o texto a evaluar.
-// Si cumple con la expresion regular devolvera como resultado un booleano true, caso contrario false.
-regex.test('Matematicas') // true
-regex.test('   ') // false
-
-
-// Metodos de arrays
-let list = [];
-
-// push: Se utiliza para insertar un elemento en la ultima posicion del array.
-list.push(1); // [1]
-
-// unshift: Caso contrario al push, inserta un elemento pero esta vez en la primera posición.
-list.unshift(0); // [0, 1]
-
-// splice: Cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
-// En este caso lo utilizaremos para eliminar un item en x posicion.
-// Recibe como primer parametro el indice del elemento a eliminar, y como segundo parametro un 1
-// que significa el número de elementos que deseas eliminar a partir de ese índice (en este caso, solo uno)
-
-list.splice(0, 1) // [1]
-
-// Recorrer un array con for
-
-// Se declara la variable "i" y se incicializa en 0, esta variable se usara como un contador para recorrer el array.
-// La condicional "i < list.length" controla cuando el bucle debe detenerse.
-// Despues de cada iteracion del buble, incrementamos el valor de "i" con i++.
-
-for (let i = 0; i < list.length; i++) {
-
-    // Haciendo uso del contador "i", vamos recorriendo las posiciones del array.
-    console.log(list[i])
+let tareas = [] 
+let eleccion = parseInt(prompt(" 0-Salir \n 1- Escribir Tarea \n 2- Listar Tareas \n 3- Editar Tareas \n 4- Eliminar Tareas"));
+function AddTarea(nombre) {
+    switch (true) {
+        case (nombre === " "):
+            alert("No se puede ingresar una tarea vacia.")
+            break;
+        case ((nombre.charAt(0) == " ")):
+            alert("No se puede agragar una tarea con espacio al principio")
+            break;
+        case ((nombre.charAt(nombre.length -1) == " ")):
+            alert("No se puede agregar una tarea con espacio al final.")
+            break
+        default:
+            tarea.push(nombre)
+            break;
+    }
 }
