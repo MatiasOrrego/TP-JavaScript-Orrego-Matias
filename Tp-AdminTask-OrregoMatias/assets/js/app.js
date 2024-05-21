6d8bd98a9d8aa10c81f2160b5a3d8a226cc93036
MatiasOrrego <orregomatias50@gmail.com>
2024-05-20 22:08:17 -0300

    Logre añadir la funcion eliminar tarea.

--- Tp-AdminTask-OrregoMatias/assets/js/app.js
+++ Tp-AdminTask-OrregoMatias/assets/js/app.js
@@ -1,5 +1,27 @@
 let tareas = [] 
-let eleccion = parseInt(prompt(" 0-Salir \n 1- Escribir Tarea \n 2- Listar Tareas \n 3- Editar Tareas \n 4- Eliminar Tareas"));
+let eleccion = 1 
+while (eleccion !== 0){
+    eleccion = parseInt(prompt(" 0-Salir \n 1- Escribir Tarea \n 2- Listar Tareas \n 3- Editar Tareas \n 4- Eliminar Tareas"));
+    switch (eleccion){
+        case 0:
+            alert("Gracias, Hasta Luego.");
+            break;
+        case 1:
+            AddTarea(prompt("Ingrese la tarea:"));
+            break;
+        case 2:
+            listTarea();
+            break;
+        case 3:
+            editTarea(prompt("Nombre de la tarea que desee editar:"));
+        case 4:
+            deletTarea(prompt("Nombre de la Tarea que desee eliminar:"));
+            break;
+        default:
+            alert("Ingrese alguna de las opciones anteriores")
+            break;
+    }
+}
 function AddTarea(nombre) {
     switch (true) {
         case (nombre === " "):
@@ -40,8 +62,15 @@ function editTarea(nombre){
                 alert("Se cambio la tarea" + nombre + "-->" + nuevonombre);
                 tareas[tareas.indexOf(nombre)] = nuevonombre;
         }
-    else{
+    } else{
         alert("Esa Tarea no existe")
     }
+}
+function deletTarea(nombre){
+    if(tareas.includes(nombre)){
+        alert("Se elimino la tarea:" + nombre);
+        tareas.splice(tareas.indexOf(nombre),1)    
+    }else{
+        alert("No existe esa Tarea.")
     }
 }
