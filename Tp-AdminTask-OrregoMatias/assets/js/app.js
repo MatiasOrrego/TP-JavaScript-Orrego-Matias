82e2d7dbba31b8fadd888263bdf65dd9185ed65c
MatiasOrrego <orregomatias50@gmail.com>
2024-05-20 21:36:58 -0300

    Logre añadir dos funciones mas, listar y editar tarea

--- Tp-AdminTask-OrregoMatias/assets/js/app.js
+++ Tp-AdminTask-OrregoMatias/assets/js/app.js
@@ -16,3 +16,32 @@ function AddTarea(nombre) {
             break;
     }
 }
+function listTarea() {
+    if (tareas.length <= 0) {
+        alert("No hay tareas.")
+    } else {
+        alert("Tareas:\n"+tareas.join("\n"))
+    }
+}
+function editTarea(nombre){
+    if (tareas.includes(nombre)) {
+        let nuevonombre = prompt ("Ingrese un nuevo nombre para la tarea.")
+        switch (true) {
+            case (nuevonombre === " "):
+                alert("No se puede ingresar una tarea vacia.")
+                break;
+            case ((nuevonombre.charAt(0) == " ")):
+                alert("No se puede agragar una tarea con espacio al principio")
+                break;
+            case ((nuevonombre.charAt(nuevonombre.length -1) == " ")):
+                alert("No se puede agregar una tarea con espacio al final.")
+                break;
+            default:
+                alert("Se cambio la tarea" + nombre + "-->" + nuevonombre);
+                tareas[tareas.indexOf(nombre)] = nuevonombre;
+        }
+    else{
+        alert("Esa Tarea no existe")
+    }
+    }
+}
